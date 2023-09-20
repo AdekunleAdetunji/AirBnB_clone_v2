4e1062eb73ef8c75af52fa371af7ff760560e880
Olawole Olawale Martins <olawalemart@gmail.com>
2023-09-20 13:17:31 +0100

    still error

--- models/base_model.py
+++ models/base_model.py
@@ -4,6 +4,7 @@ import uuid
 from datetime import datetime
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, String, DateTime
+import models
 
 
 Base = declarative_base()
@@ -27,6 +28,7 @@ class BaseModel:
         else:
             self.id = str(uuid.uuid4())
             self.created_at = self.updated_at = datetime.now()
+            models.storage.new()
 
     def __str__(self):
         """Returns a string representation of the instance"""
