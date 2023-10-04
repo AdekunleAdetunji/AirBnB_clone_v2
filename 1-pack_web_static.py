7e9abbf6f536851564c1de66fa36ce839d39f221
adetunji <adekunleadetunjiwilson@gmail.com>
2023-10-05 00:03:17 +0100

    replaced os module with pathlib

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -5,17 +5,16 @@ archive from the contents of the web_static folder of your AirBnB Clone repo,
  using the function do_pack
 """
 
-import os
 from datetime import datetime
 from fabric.api import local
-
+from pathlib import Path
 
 def do_pack():
     """
     A function that is used to generate a tar file using the local function
     """
-    if not os.path.exists("versions"):
-        os.mkdir("versions")
+    if not Path("versions").exists:
+        Path.mkdir("versions")
     time = datetime.now().strftime("%Y%m%d%H%M%S")
     filePath = f"versions/web_static_{time}.tgz"
     output = local(f"tar -cvzf {filePath} web_static")
