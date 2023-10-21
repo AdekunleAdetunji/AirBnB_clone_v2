bc4d14758a2a6090a37783b2f839512d37e3dfb0
adetunji <adekunleadetunjiwilson@gmail.com>
2023-10-21 12:40:33 +0100

    reformatted the html template

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -6,7 +6,17 @@ storage
 from flask import Flask, render_template
 from models import storage
 from models.state import State
+
+
 app = Flask(__name__)
+
+
+@app.route("/states_list", strict_slashes=False)
+def states_list():
+    """view executed when the /states_list is queried"""
+    states = storage.all(State).values()
+    return render_template("7-states_list.html",
+                           states=states)
 
 
 @app.teardown_appcontext
@@ -15,12 +25,5 @@ def close(exception=None):
     storage.close()
 
 
-@app.route("/states_list", strict_slashes=False)
-def states_list():
-    """view executed when the /states_list is queried"""
-    states = storage.all(State)
-    return render_template("7-states_list.html",
-                           states=states)
-
-
-app.run(host="0.0.0.0", port=5000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
